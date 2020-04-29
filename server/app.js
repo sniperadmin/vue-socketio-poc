06a099e72d76af8db367ffe06ceec67cc7abb7cd
Nasr Galal <nasrbue@gmail.com>
2020-04-29 23:05:49 +0200

    code update

--- server/app.js
+++ server/app.js
@@ -10,7 +10,22 @@ const io = require('socket.io')(server);
 io.on('connection', socket => {
   console.log(socket.id)
 
-  socket.on('SEND_MESSAGE', data => {
-    io.emit('MESSAGE', data)
+  socket.on('SEND_MESSAGE', (data, room) => {
+    io.to(room).emit('MESSAGE', data);
+  });
+
+  socket.on('JOIN_ROOM', data => {
+    console.log('User joined room', data);
+    socket.join(data);
+    // io.sockets.emit('update', { message: socket.client.server.clients, type: 'online' });
+  })
+  
+  socket.on('LEAVE_ROOM', data => {
+    console.log('User joined room', data);
+    socket.leave(data);
+  })
+  
+  socket.on('disconnect', () => {
+    // io.sockets.emit('update', { message: socket.client.server.clients, type: 'online' });
   })
 })
--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="card mt-3">
-      <div class="card-body">
+      <div v-if="!showBoard" class="card-footer">
+          <form @submit.prevent="sendMessage">
+              <div class="form-group">
+                  <label for="user">User:</label>
+                  <input type="text" v-model="message.user" class="form-control">
+              </div>
+              <button @click="joinRoom('general')" type="submit" class="btn btn-success">Join chat</button>
+          </form>
+      </div>
+
+      <section v-if="showBoard">
+
+        <div class="card-body">
           <div class="card-title">
-              <h3>Chat Group</h3>
+              <h3>{{ room ? room : 'Chat App' }}</h3>
               <hr>
           </div>
           <div class="card-body">
-            <div
+              <div
                 v-for="m in messages"
                 :key="m.user"
                 class="messages"
@@ -14,20 +26,38 @@
                 {{ m.message.text }}
               </div>
           </div>
-      </div>
-      <div class="card-footer">
-          <form @submit.prevent="sendMessage">
-              <div class="gorm-group">
-                  <label for="user">User:</label>
-                  <input type="text" v-model="message.user" class="form-control">
-              </div>
-              <div class="gorm-group pb-3">
-                  <label for="message">Message:</label>
-                  <input type="text" v-model="message.text" class="form-control">
-              </div>
-              <button type="submit" class="btn btn-success">Send</button>
-          </form>
-      </div>
+        </div>
+        <div class="card-footer">
+            <form @submit.prevent="sendMessage">
+                <div class="form-group">
+                    <label for="user">User:</label>
+                    <input type="text" v-model="message.user" class="form-control">
+                </div>
+                <div class="gorm-group pb-3">
+                    <label for="message">Message:</label>
+                    <input type="text" v-model="message.text" class="form-control">
+                </div>
+                <button type="submit" class="btn btn-success">Send</button>
+            </form>
+        </div>
+            
+
+        <div class="card">
+            <div class="card-title">Rooms</div>
+            <div class="card-body">
+                <div @click="joinRoom('general')" class="gtitle">
+                    <p>
+                        general
+                    </p>
+                </div>
+                <div @click="joinRoom('men')" class="gtitle">
+                    <p>
+                        men
+                    </p>
+                </div>
+            </div>
+        </div>
+      </section>
   </div>
 </template>
 
@@ -43,12 +73,18 @@ export default {
                 text: ''
             },
             messages: [],
-            socket: io('http://localhost:3001')
+            socket: io('http://localhost:3001'),
+            room: '',
+            showBoard: false,
         }
     },
     mounted() {
+        // this.joinRoom('general')
         this.socket.on('MESSAGE', data => {
             this.messages = [...this.messages, data]
+        })
+        this.socket.on('update', data => {
+            console.log(data.message)
         })
     },
     methods: {
@@ -57,8 +93,18 @@ export default {
 
             this.socket.emit('SEND_MESSAGE', {
                 message: this.message
-            })
+            }, this.room)
             this.message.text = ''
+        },
+        joinRoom(roomName) {
+            this.showBoard = true
+            this.messages = []
+            if (this.room != '') {
+                this.socket.emit('LEAVE_ROOM', this.room)
+            }
+
+            this.socket.emit('JOIN_ROOM', roomName)
+            this.room = roomName
         }
     }
 }
